c3a7e826e15b4661e4714d453cd80de7b605347a
superdev <ctori0816@gmail.com>
2024-04-19 02:33:18 -0700

    s

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
-import path from 'path'
-import express from 'express'
-import dotenv from 'dotenv'
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import cookieParser from 'cookie-parser';
-import session from 'express-session'
-import connectDB from './config/db.js'
-import estateRoutes from './routes/estateRoutes.js'
-import userRoutes from './routes/userRoutes.js'
-import uploadsRoutes from './routes/uploadsRoutes.js'
-import favouriteRoutes from './routes/favouriteRoutes.js'
-import postAtRoutes from './routes/postAtRoutes.js'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import connectDB from "./config/db.js";
+import estateRoutes from "./routes/estateRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import uploadsRoutes from "./routes/uploadsRoutes.js";
+import favouriteRoutes from "./routes/favouriteRoutes.js";
+import postAtRoutes from "./routes/postAtRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 connectDB(); // MongoDB connect
 
@@ -20,42 +19,40 @@ const app = express();
 
 // Body parser middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
+app.use(express.urlencoded({ extended: true }));
 
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.use(session({secret: 'blahblahblah'}));
-
-
+app.use(session({ secret: "blahblahblah" }));
 
 app.use("/api/estates", estateRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/favourites", favouriteRoutes);
 app.use("/api/postat", postAtRoutes);
-app.use('/api/upload', uploadsRoutes);
+app.use("/api/upload", uploadsRoutes);
 
 const __dirname = path.resolve();
 
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === 'production') {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static('/var/data/uploads'));
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+if (process.env.NODE_ENV === "production") {
+  // const __dirname = path.resolve();
+  // app.use("/uploads", express.static("/var/data/uploads"));
+  app.use("/uploads", express.static(path.join(__dirname, "/data/uploads")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
   );
 } else {
-  const __dirname = path.resolve();
-  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
+  // const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/", (req, res) => {
+    res.send("API is running....");
   });
 }
-
 
 app.use(notFound);
 app.use(errorHandler);
